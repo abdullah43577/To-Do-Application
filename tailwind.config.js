9b07e513e7ae85ef49161b0359434c77f3662112
Abdullah Ayoola <100965547+abdullah43577@users.noreply.github.com>
2023-01-09 17:56:44 +0100

    finished setting up my codespace

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+
+    extend: {
+      colors: {
+        veryDarkBlue: "hsl(235, 21%, 11%)",
+        veryDarkDesaturatedBlue: "hsl(235, 24%, 19%)",
+        lightGrayishBlue: "hsl(234, 39%, 85%)",
+      },
+    },
   },
   plugins: [],
 };
